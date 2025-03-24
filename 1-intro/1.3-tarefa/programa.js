da4b7d396426192d7c138d669812bb019ca809ad
Gabriel Pivoto <gabrielspivoto@gmail.com>
2025-03-24 14:09:49 -0300

    add 1.2 and homework

---
+++ 1-intro/1.3-tarefa/programa.js
@@ -0,0 +1,13 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Digite dois números separados por espaço: ', (input) => {
+  const [a, b] = input.split(' ').map(Number);
+  const c = a + b;
+  console.log(`A soma dos valores é: ${c}`);
+  rl.close();
+});
